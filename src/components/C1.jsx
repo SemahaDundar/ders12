c96fea4639ee4554dd892f5cf5f32eff0ae5bb15
SemahaDundar <semahadundaruyar94@gmail.com>
2022-08-01 21:42:33 +0300

    react css örneklerine bakıldı

---
+++ src/components/C1.jsx
@@ -0,0 +1,26 @@
+import  './C1.css';
+
+
+
+function C1(){
+
+
+
+    const paragrafStili = {
+
+        backgroundColor: "yellow",
+        color: "green",
+        padding: "1rem"
+    };
+
+    return(
+        <>
+        <p style={paragrafStili} className='ozel-paragraf'>Lorem ipsum dolor sit, amet consectetur adipisicing e<br></br>
+            lit. Molestiae tempore dignissimos fuga porro veniam. Facilis cum fuga quos praesentium dolor illo debitis,
+             eveniet ducimus iure doloribus nam nobis inventore dolores!</p>
+        
+        </>
+    )
+}
+
+export default C1;

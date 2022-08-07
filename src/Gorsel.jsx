c8281597654fb4d746be03277a3fcf9f15ee6341
SemahaDundar <semahadundaruyar94@gmail.com>
2022-08-07 16:50:00 +0300

    useeffect ile görsel çalışması

---
+++ src/Gorsel.jsx
@@ -0,0 +1,15 @@
+function Gorsel(gorsel){
+
+
+    return(
+        <div>
+
+            <img src={gorsel.download.url} alt="" />
+            <h2>{gorsel.author}</h2>
+        </div>
+    )
+
+}
+
+
+export default Gorsel;
